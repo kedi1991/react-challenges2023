c29831a075df1e270592352158b808a6c0bc5deb
okurut <henrykedi.okurut@netcompany-intrasoft.com>
2023-09-16 02:48:23 +0200

    Add call back function to the set state method

--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -12,10 +12,22 @@ constructor(props){
     }
 }
 
+    handleClick(){
+        //add a call back function to prevent async nature of the setState
+        this.setState({
+            introduction: "Goodbye!",
+            buttonText: "Login"
+        }, () => {
+            console.log("button clicked after change of state");
+
+        });  
+        console.log("button clicked");
+    }
+
     render(){
         return <div>
             <h1>{this.state.introduction} {this.props.myname}</h1>
-            <button>{this.state.buttonText}</button>
+            <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
         </div>
     }
 }

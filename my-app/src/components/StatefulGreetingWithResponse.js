0d0549e97c8553834a0c42217216ac175c1dd3c0
okurut <henrykedi.okurut@netcompany-intrasoft.com>
2023-09-16 04:18:30 +0200

    Add incremental number and states

--- my-app/src/components/StatefulGreetingWithResponse.js
+++ my-app/src/components/StatefulGreetingWithResponse.js
@@ -6,14 +6,16 @@ class StatefulGreetingWithRespose extends React.Component{
         super(props)
         this.state = {
             greeting:"Hello",
-            buttonText:"Login"
+            buttonText:"Login",
+            counter: 0
         }
     }
 
     manageClick = () => {
         this.setState({
             greeting: this.state.greeting === "Bye!"? "Hello": "Bye!",
-            buttonText: this.state.buttonText === "Logout"? "Login": "Logout"
+            buttonText: this.state.buttonText === "Logout"? "Login": "Logout",
+            counter: this.state.counter+=1
         }, () => {
             console.log("you just clicked me......")
 
@@ -25,6 +27,7 @@ class StatefulGreetingWithRespose extends React.Component{
         return <div>
             <h1>{this.state.greeting}</h1>
             <button onClick={this.manageClick}>{this.state.buttonText}</button>
+            <button>{this.state.counter}</button>
         </div>
      }
 }

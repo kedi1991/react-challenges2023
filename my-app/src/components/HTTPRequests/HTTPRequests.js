85526645ce4547be7a47c4a933c13478ec77f0a7
okurut <henrykedi.okurut@netcompany-intrasoft.com>
2023-09-23 02:56:52 +0200

    Add the map function to diplay all info in rows

--- my-app/src/components/HTTPRequests/HTTPRequests.js
+++ my-app/src/components/HTTPRequests/HTTPRequests.js
@@ -4,38 +4,39 @@ import React, { Component } from 'react'
 export default class HTTPRequests extends Component {
 
     constructor(props) {
-      super(props)
-    
-      this.state = {
-        posts: []
-      }
+        super(props)
 
-      
+        this.state = {
+            posts: []
+        }
     }
     //get the data after loading
-    componentDidMount(){
+    componentDidMount() {
         //then call the API
-        axios.get("https://jsonplaceholder.org/posts").then(
+        axios.get("https://jsonplaceholder.org/posts/1").then(
             response => {
-                console.log(response)
+                console.log(response);
                 this.setState({
                     posts: response.data
                 })
             }
         )
-
-        
-
-        
     }
 
-  render() {
-    return (
-      <div>
-        <strong>Posts:</strong>
-        <p>{JSON.stringify(this.state.posts)}</p>
-      </div>
-      
-    )
-  }
+    render() {
+        const posts = this.state.posts;
+        return (
+            <div>
+                <h1><strong/>Post:<strong/></h1>
+                {posts.map(item => (
+                    <div>
+                    <h3>{item.id}.{item.title}</h3>
+                    <h4>{item.content}</h4>
+                    <img src='{item.image}'></img>
+                    <hr></hr>
+                    </div>
+                )) }
+            </div>
+        )
+    }
 }
